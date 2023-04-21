a869c1d5a8e2beb4612d4cb7b1ab7e8468e14f21
= <=>
2023-04-21 18:17:43 -0300

    Primeiro Commit

---
+++ src/main/java/com/rcm/sistemas/arquiteturahexagonal/ArquiteturaHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.rcm.sistemas.arquiteturahexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/rcm/sistemas/arquiteturahexagonal/application/core/usecase/InsertCustomerUseCase.java
@@ -0,0 +1,8 @@
+package com.rcm.sistemas.arquiteturahexagonal.application.core.usecase;
+
+public class InsertCustomerUseCase {
+
+    public void insert(){
+
+    }
+}
