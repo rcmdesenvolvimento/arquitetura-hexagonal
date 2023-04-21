9318056afc2e4f76a56f88a4348b12bea49779eb
= <=>
2023-04-21 18:52:20 -0300

    Criado o Client

--- src/main/java/com/rcm/sistemas/arquiteturahexagonal/ArquiteturaHexagonalApplication.java
+++ src/main/java/com/rcm/sistemas/arquiteturahexagonal/ArquiteturaHexagonalApplication.java
@@ -2,8 +2,10 @@ package com.rcm.sistemas.arquiteturahexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ArquiteturaHexagonalApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/rcm/sistemas/arquiteturahexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -0,0 +1,17 @@
+package com.rcm.sistemas.arquiteturahexagonal.adapters.out.client;
+
+import com.rcm.sistemas.arquiteturahexagonal.adapters.out.client.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "FindAddressByZipCodeClient",
+        url = "${ricardo.client.address.url}"
+)
+public interface FindAddressByZipCodeClient {
+
+    @GetMapping("/{zipCode}")
+    AddressResponse find(@PathVariable("zipCode") String zipCode);
+
+}
